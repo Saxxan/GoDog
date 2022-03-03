fcdf734584ade6d68e4bfdec5d370c85f573b495
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-03-03 11:41:08 +0100

    icons uploaded

---
+++ php/icons/icon-arrow.php
@@ -0,0 +1,5 @@
+<!-- Arrow svg icon -->
+
+<svg width="48" height="48" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <path fill-rule="evenodd" clip-rule="evenodd" d="M46 26.7778H21V21.2222H46V26.7778ZM12.6667 21.2222H15.5V26.7778H12.6667L27.9444 42.0556L24 46L2 24L22 4L27.9444 5.94444L12.6667 21.2222Z" fill="black"/>
+</svg>
