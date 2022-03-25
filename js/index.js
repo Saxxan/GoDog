b601a06413802bc4fec2ccf87a837c8bd9930e4f
Salvador Puche Ibáñez <78732743+Saxxan@users.noreply.github.com>
2022-03-25 20:44:52 +0100

    function filter users and print card

--- js/index.js
+++ js/index.js
@@ -1,6 +1,42 @@
 // -------- ARCHIVO JS RECOPILATORIO FUNCIONES
 
-const usersURL = './users.json';
+const usersURL = '/js/users.json';
+const cards = document.querySelector('.cards__container');
+var cp = '';
+var from = '';
+var to = '';
+var services = '';
+var servicio = '';
+var btnSubmit = document.querySelector('.form__input__submit');
+let users = [];
+
+// ******************************
+// Function print users card
+// ******************************
+
+function printCard(user){
+    cards.innerHTML = `
+    <article class="card">
+        <main class="card__body">
+            <img src="" alt="" class="card__body__photo">
+            <div class="card__body__text">
+                <h6 class="card__body__text__title">${user.name}</h6>
+                <p class="card__body__text__date">From ${user.from} to ${user.to}</p>
+                <p class="card__body__text__cp">${user.cp}</p>
+                <p class="card__body__text__info"></p>
+            </div>
+            <aside class="card__body__marks">
+                <svg class="star--icon"></svg>
+                <p class="card__body__marks__mark"></p>
+            </aside>
+        </main>
+        <footer class="card__footer">
+            <p class="card__footer__price">${user.price}</p>
+            <button class="card__footer__contact"></button>
+        </footer>
+    </article>
+    `
+}
 
 // ****************************
 // Function get users
@@ -10,28 +46,29 @@ function getUsers(){
     
     axios.get(usersURL)
         .then(function(response) {
-            let users = response.data;
-            console.log(users);
+            users = response.data;
+            
+            for(var i=0; i<users.length; i++) {
+                const cpTrue = cp == '' || users[i].cp == cp;
+                const fromTrue = from == '' || users[i].from == from;
+                const toTrue = to == '' || users[i].to == to;
+                const servicioTrue = true;
+        
+                console.log(cpTrue);
+                console.log(fromTrue);
+                console.log(toTrue);
+        
+                if(cpTrue && fromTrue && toTrue && servicioTrue){
+                    printCard(users[i]);
+                }
+            };
         });
-
-    // fetch('./users.json')
-    //     .then(function(response) { return response.json(); })
-    //     .then(function(users){
-    //         console.log(users);
-    //     })
 }
 
 
 // **********************************
 // Get input values from form
 // **********************************
-
-var cp = '';
-var from = '';
-var to = '';
-var services = '';
-var servicio = '';
-var btnSubmit = document.querySelector('.form__input__submit');
 
 btnSubmit.addEventListener('click', function(){
     cp = document.getElementById('cp').value;
@@ -57,41 +94,3 @@ btnSubmit.addEventListener('click', function(){
 
     debugger;
 })
-
-
-
-// ******************************
-// Function print users card
-// ******************************
-
-const cards = document.querySelector('.cards__container');
-
-function printCard (){
-    axios.get('./users.json')
-        .then(function(response) {
-            let users = response.data;
-            console.log(users);
-            debugger;
-            cards.innerHTML = `
-            <article class="card">
-                <main class="card__body">
-                    <img src="" alt="" class="card__body__photo">
-                    <div class="card__body__text">
-                        <h6 class="card__body__text__title"></h6>
-                        <p class="card__body__text__date"></p>
-                        <p class="card__body__text__cp"></p>
-                        <p class="card__body__text__info"></p>
-                    </div>
-                    <aside class="card__body__marks">
-                        <svg class="star--icon"></svg>
-                        <p class="card__body__marks__mark"></p>
-                    </aside>
-                </main>
-                <footer class="card__footer">
-                    <p class="card__footer__price"></p>
-                    <button class="card__footer__contact"></button>
-                </footer>
-            </article>
-            `
-        })
-}
